51c542c1c8e28109f214d59c3874196473cf282b
Hatice <haticekaraty@gmail.com>
2021-10-09 23:14:43 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Hatice", email: "email@email.com", password: '1234')
+puts "User created"
+
+Income.create(name: "Teaching", amount: 3000.0, user_id:1)
+Income.create(name: "Passive", amount: 120.0, user_id:1)
+Income.create(name: "Tutoring", amount: 200.0, user_id:1)
+
+puts "Income seed data is created"
+
+Expense.create(name: "Rent", amount: 1400.0, budget: 1400.0 ,user_id:1)
+Expense.create(name: "Utilities", amount: 200.0, budget: 200.0 ,user_id:1)
+Expense.create(name: "Groceries", amount: 300.0, budget: 400.0 ,user_id:1)
+Expense.create(name: "Hospital", amount: 100.0, budget: 150.0 ,user_id:1)
+Expense.create(name: "Subscriptions", amount: 40.0, budget: 40.0 ,user_id:1)
+Expense.create(name: "Gym", amount: 60.0, budget: 60.0 ,user_id:1)
+Expense.create(name: "Fun", amount: 80.0, budget: 100.0 ,user_id:1)
+
+
+puts "Expense seed data is created"
+
+
+
+
+
