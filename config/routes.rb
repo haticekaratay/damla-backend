c6ffacf7217f23ddea305f77b5758d1aa38f462a
Hatice <haticekaraty@gmail.com>
2021-10-09 23:15:53 -0400

    add post login to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
   resources :expenses
   resources :incomes
-  resources :users
+  resources :users 
+
+  post "/login" => "auth#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
