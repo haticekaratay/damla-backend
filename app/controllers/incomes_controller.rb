6980a78153482ecc5a05721e8d868bfdcd0bd86f
Hatice <haticekaraty@gmail.com>
2021-10-10 21:42:56 -0400

    add create and index actions in incomes controller

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,2 +1,19 @@
 class IncomesController < ApplicationController
+    # before_action :authorized
+
+    def index
+        #byebug
+        incomes = current_user.incomes
+        render json: incomes, except: [:created_at, :updated_at]
+    end
+
+    def create
+        income = Income.new(user_id:current_user.id, name: params[:name], amount:params[:amount])
+        if income.save
+            render json: income, except: [:created_at, :updated_at]
+        else
+            render json: { error: "Try again."}#, status: :unauthorized #status: :unprocessable_entity
+        end
+    end
+
 end
