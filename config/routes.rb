7ba0ede465f8793432ad587e999a2531fd410162
Hatice <haticekaraty@gmail.com>
2021-10-10 21:40:40 -0400

    get current user route is added

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users 
 
   post "/login" => "auth#create"
+  get "/get_current_user" => "auth#get_current_user"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
