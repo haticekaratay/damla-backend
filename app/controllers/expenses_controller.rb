76b6ac6ab9196083485c70eeb759652b69cd0439
Hatice <haticekaraty@gmail.com>
2021-10-12 18:52:47 -0400

    render current user expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,7 @@
 class ExpensesController < ApplicationController
+    def index
+        expenses = current_user.expenses
+        render json: expenses, except: [:created_at, :updated_at]
+    end
+
 end
