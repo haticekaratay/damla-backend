5ae0de2ff7bde2e3d3003ee35a7556b0c5c350a7
Hatice <haticekaraty@gmail.com>
2021-10-17 14:31:49 -0400

    add create expense action

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,4 +4,13 @@ class ExpensesController < ApplicationController
         render json: expenses, except: [:created_at, :updated_at]
     end
 
+    def create
+        expense = Expense.new(user_id: current_user.id, name:params[:name], budget:params[:budget])
+        if expense.save
+            render json: expense, except: [:created_at, :updated_at]
+        else 
+            render json: { error: "Add expense has failed. Try again"}
+        end
+    end
 end
+  
