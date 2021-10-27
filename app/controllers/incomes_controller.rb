0be7575375ab0ada5833d598fc4bc890cb6a068b
Hatice <haticekaraty@gmail.com>
2021-10-27 17:23:46 -0400

    clean up from redundant code

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -2,7 +2,6 @@ class IncomesController < ApplicationController
      before_action :authorized
 
     def index
-        #byebug
         incomes = current_user.incomes
         render json: incomes, except: [:created_at, :updated_at]
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  
   resources :expenses
   resources :incomes
   resources :users 
-
   post "/login" => "auth#create"
   get "/get_current_user" => "auth#get_current_user"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
