94ce4397974985756b4e0f12e2370c453381f9c1
Hatice <haticekaraty@gmail.com>
2021-10-27 18:14:56 -0400

    fix the bug in url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://damla-budget.netlify.app/'
+    origins 'https://damla-budget.netlify.app'
 
     resource '*',
       headers: :any,
